7c19ec92f2ae89c24c34b6e9be02c6f57bea87e4
yourvita <youjinseozzang@gmail.com>
2022-09-14 17:01:45 +0900

    🚧 Implement toggling order-form-modal

---
+++ js/order-cta.js
@@ -0,0 +1,18 @@
+const orderCta = document.querySelector('.order-cta')
+const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
+// 아래 두줄과 위 한줄 코드는 같은 코드이다. DOM 을 이용한 요소 조작법
+// const orderCtaBuyButton = orderCta.children[0]
+// const orderCtaBookmarkButton = orderCta.children[1]
+
+const orderModal = document.querySelector('.order-form-modal')
+const orderModalOverlay = document.querySelector('.overlay')
+
+orderCtaBuyButton.addEventListener('click', function () {
+  orderModal.classList.add('is-open')
+  orderModalOverlay.classList.add('is-active')
+})
+
+orderModalOverlay.addEventListener('click', function () {
+  orderModal.classList.remove('is-open')
+  orderModalOverlay.classList.remove('is-active')
+})
