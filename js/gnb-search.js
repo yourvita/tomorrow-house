4c6740915927a3553508ead0d56e31a3d56aadd8
yourvita <youjinseozzang@gmail.com>
2022-09-15 17:59:26 +0900

    🚧Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -6,12 +6,16 @@ const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
 const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
-console.log(gnbSearchHistoryList)
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
 
-function closeGnbSearchHistory(e) {
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+}
+
+function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistory)
+    closeGnbSearchHistory()
   }
 }
 
@@ -19,12 +23,11 @@ function openGnbSearchHistory() {
   // 체크 => gnbSearchHistoryList 안에 li가 몇개가 있는지
   // li가 0개 => 실행 x
 
-  console.log(gnbSearchHistoryList.children)
   if (gnbSearchHistoryList.children.length === 0) {
     return
   }
   if (!gnbSearchHistory.classList.contains('is-active')) {
-    window.addEventListener('click', closeGnbSearchHistory)
+    window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
   gnbSearchHistory.classList.add('is-active')
 }
@@ -34,6 +37,23 @@ gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 function deleteAllSearchHistoryItems() {
   // gnbSearchHistoryList 안에 들어있는 모든 li를 삭제해!
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
 }
+
 deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
+
+function deleteSearchHsitoryItem(e) {
+  e.stopPropagation()
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
+
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHsitoryItem)
+})
+
+// removechild를 사용할 때 어떤 요소를 지우고자할때 그 권한은 요소의 부모요소에게만 있다.
